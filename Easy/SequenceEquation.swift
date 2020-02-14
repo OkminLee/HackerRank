ac7d7d830a7953bcbb8dc32f77a410e243a92adb
OkminLee <leeokmin@gmail.com>
2020-02-14 10:15:07 +0900

    Update SequenceEquation.swift

--- Easy/SequenceEquation.swift
+++ Easy/SequenceEquation.swift
@@ -1,1 +1,8 @@
-HackerRank
+//https://www.hackerrank.com/challenges/permutation-equation/problem
+
+func permutationEquation(p: [Int]) -> [Int] {
+    let x = p.sorted()
+    let mapped = x.map { p.firstIndex(of: $0)! + 1 }
+    let result = mapped.map { p.firstIndex(of: $0)! + 1}
+    return result
+}
