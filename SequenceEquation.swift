92218f40a9d9e73f741f8be06e2c9c021d061254
OkminLee <leeokmin@gmail.com>
2020-02-25 08:37:31 +0900

    Delete SequenceEquation.swift

--- SequenceEquation.swift
+++
@@ -1,6 +0,0 @@
-func permutationEquation(p: [Int]) -> [Int] {
-    let x = p.sorted()
-    let mapped = x.map { p.firstIndex(of: $0)! + 1 }
-    let result = mapped.map { p.firstIndex(of: $0)! + 1}
-    return result
-}
