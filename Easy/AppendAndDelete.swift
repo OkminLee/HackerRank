e0131211bbf5747e8fdfe5efa7c1ed50358a2dc9
Okmin <leeokmin@gmail.com>
2020-08-31 17:04:17 +0900

    Update AppendAndDelete.swift

--- Easy/AppendAndDelete.swift
+++ Easy/AppendAndDelete.swift
@@ -1,3 +1,4 @@
+//https://www.hackerrank.com/challenges/append-and-delete/problem
 func appendAndDelete(s: String, t: String, k: Int) -> String {
     var sArray = Array(s)
     var tArray = Array(t)
@@ -6,7 +7,6 @@ func appendAndDelete(s: String, t: String, k: Int) -> String {
     var k = k
     for i in 0..<maxCount {
         if i == minCount || sArray[i] != tArray[i] {
-            print(i)
             if s.count < t.count {
                 k = k - i*2
             }
@@ -18,7 +18,6 @@ func appendAndDelete(s: String, t: String, k: Int) -> String {
     if sArray == tArray {
         sArray.removeAll()
     }
-    print(sArray, tArray)
     
     let condition = sArray.count + tArray.count <= k || (s == t && k % 2 == 0) || (Set(s) == Set(t) && k % 2 == 0)
     if condition {
